30ae2a9287cba1998375374ad046e74fca6342b7
Rizky Maulana <rizkym71338@gmail.com>
2022-10-15 10:40:43 +0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section className="h-screen flex items-center justify-center flex-col bg-slate-50">
+        <h1 className="text-3xl font-bold text-slate-900">Homepage</h1>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <section className="h-screen flex items-center justify-center flex-col bg-slate-50">
+        <h1 className="text-3xl font-bold text-slate-900">404 Notfound</h1>
+      </section>
+    </>
+  );
+};
+
+export default NotFound;
